0b838e1e8c11a483601ad4a7091941666245ec70
iteesha22 <jindalteesha22@gmail.com>
2025-06-08 22:37:24 +0530

    matchcase added

--- matchcase.py
+++ matchcase.py
@@ -24,6 +24,21 @@ The matchcase consists of three main entities:
 3. default clause(expression for each case): This is the clause that is executed if none of the patterns match.'''
  # example:
 
+x = int(input("Enter a number: "))
+match x:
+    case 0:
+        print("You entered Zero")
+    case 1:
+        print("You entered one")
+    case 2:
+        print("You entered two")
+    case _:
+        print(x, " is Not zero")
+
+         # This is the default clause, executed if none of the above cases match.
+
+# with conditions: 
+
 x=int(input(" Enter a number"))
 # x is a variable to match.
 match x:
@@ -33,12 +48,25 @@ match x:
         print("You entered one")
     case 4:
         print("You entered four")
+    case _ if x==60:
+        print(x,"is 60") 
+    case _ if x!=90:
+        print(x," is not 90")
+    case _ if x!=80:
+        print(x," is not 80")
     case _:
         print("You entered something else")  # This is the default clause, executed if none of the above cases match.
 
-
-              
-
+# OUICK NOTE:
+'''In a match statement, only the first matching case is executed, 
+and it does not keep checking other case blocks afterward — even if 
+the other if guards would also be true.
+ '''
+# QUICK NOTE:
+'''Why case _ if x != 60: works but case _ if x = 60: does not?
+* x != 60 is a comparison checking if x is not equal to 60 — this is a valid boolean expression. 
+* x = 60 is an assignment, not a comparison. Assignments are not allowed 
+   in conditions because they don’t produce a boolean value.'''
 
 
 
