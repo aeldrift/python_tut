0e03fdc94add0b23977e8fbd28562e867e25dcb8
iteesha22 <jindalteesha22@gmail.com>
2025-07-21 20:50:28 +0530

    added

---
+++ dict.py
@@ -0,0 +1,44 @@
+# to create a dictionery 
+# And applying different operatiions 
+
+ep1={122:45, 123:89, 567:69, 670:69}  # dict created
+ep2={222:67, 566:90}
+
+ep1.update(ep2)   #dict2 is added to dict1 and updated
+print(ep1)  # {122: 45, 123: 89, 567: 69, 670: 69, 222: 67, 566: 90}
+
+ep1.pop(122)
+print(ep1)  # {123: 89, 567: 69, 670: 69, 222: 67, 566: 90}
+
+ep1.popitem()
+print(ep1)  # {123: 89, 567: 69, 670: 69, 222: 67}
+
+# del ep1   # will deletes the dict ep1 
+# print(ep1)  # so shows an error as now, no ep1 dict exists.
+
+del ep1[670]
+print(ep1)  # {123: 89, 567: 69, 222: 67}
+
+# Some important things to know:
+
+# SOME IMPORTANT THINGS TO KNOW:
+
+# we can use else statement with for loop and while loop also.
+
+# cse: For loop
+for i in range(6):
+  print(i)
+  if i==4:    # If else executed then loop doesn't break. It exits sucessfully.
+    break
+else:
+    print("sorry no i is there")   
+
+#case: While loop
+i=0
+while i<7:
+  print(i)
+  i=i+1
+  if i==4:
+    break
+else:
+    print("sorry no i is there")
