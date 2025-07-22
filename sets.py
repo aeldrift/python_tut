2a6ad94af8f6ff9f2165a805d410b767833942a7
Teesha <jindalteesha22@gmail.com>
2025-07-22 10:53:39 +0530

    Update sets.py

--- sets.py
+++ sets.py
@@ -42,5 +42,40 @@ print("juice set\(when not updated) is:", juice) # remains unchanged
 juice.intersection_update(juice2)
 print("when juice set updated:", juice)
 
+# Some other important operations performed
+cities={"Tokyo","Madrid","Berlin","Delhi"}
+cities2={"Seoul","Kabul"}
+cities.update(cities2)
+print(cities)
+
+cities.discard("tokyo")
+print(cities)
+print("hello world3!")
+
+cities.clear()
+print(cities)
+info={"carla",19,"harry",19.7,True}
+
+# to check if an item exists or not
+if "carla" in info:
+    print("carla is  present")
+else:
+    print("carla is absent") 
+# # item=cities.pop()
+# print(cities)          # AS IT SHOWS ERROR SO IS COMMENT OUT HERE.
+# print("popped item is:", item)
+
+x = {"apple", "banana", "cherry","google", "microsoft", "facebook"}
+y = {"google", "microsoft", "facebook","apple"}
+
+z = x.isdisjoint(y)
+print(z)
+
+z = x.issuperset(y)
+print(z)
+
+z = x.issubset(y)
+print(z)
+
 
 
