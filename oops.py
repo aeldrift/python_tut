e9bdc1e85a90a1233bd750347b177430f2dff306
Teesha <jindalteesha22@gmail.com>
2025-07-29 10:01:04 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -5,10 +5,15 @@ class person():
     name="Harry"
     occupation ="Software Developer"
     network=10
+    def info(self): # using self parameter
+        print(f"{self.name} is a {self.occupation}")
 
 a= person()
+b= person()
 a.name ="Shubham"
 a.occupation="Accountant"
-print(a.name,a.occupation)
 
-
+a.name ="Nitika"
+a.occupation="HR"
+a.info()
+b.info()
