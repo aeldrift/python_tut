c08f744abc8f947fa49493b4eac5dfb06db9168a
Teesha <jindalteesha22@gmail.com>
2025-07-29 09:46:43 +0530

    Create oops.py
    file added

---
+++ oops.py
@@ -0,0 +1,14 @@
+# OOPS CONCEPTS
+# To create classes and objects
+
+class person():
+    name="Harry"
+    occupation ="Software Developer"
+    network=10
+
+a= person()
+a.name ="Shubham"
+a.occupation="Accountant"
+print(a.name,a.occupation)
+
+
