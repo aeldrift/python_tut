b6707490081a077e1f7041f742dccacd92c6a61a
Teesha <jindalteesha22@gmail.com>
2025-07-30 15:40:27 +0530

    Update decorators.py
    decorators added

--- decorators.py
+++ decorators.py
@@ -61,4 +61,11 @@ import logging
 # Configure logging to display INFO level messages
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+def log_function_call(func):
+    def decorated(*args, **kwargs):
+        logging.info(f'Calling {func.__name__} with args={args}, kwargs={kwargs}')
+        result = func(*args, **kwargs)
+        logging.info(f'{func.__name__} returned {result}')
+        return result
+    return decorated
 
