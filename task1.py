3768bac0edbfacb7aef726d016e70bebca86b9bb
Teesha <jindalteesha22@gmail.com>
2025-08-05 10:12:02 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,9 @@
+# Task
+''' Create a Book class that manages a book's title, author, and price. Implement the following:  
+A constructor to initialize the title, author, and price of the book.
+Getter methods for title, author, and price.
+Setter methods for:
+title and author (ensure the title and author are non-empty strings).
+price (ensure the price is non-negative).
+A method discount that applies a discount (a percentage between 0 and 100) to the price.'''
+
