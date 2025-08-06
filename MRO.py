4f2cd2c3eb5fa35f36cb0af69925f31c4ac2890f
iteesha22 <jindalteesha22@gmail.com>
2025-08-06 21:35:42 +0530

    added

---
+++ MRO.py
@@ -0,0 +1,2 @@
+# MRO: Method Resolution Order
+''' this is the oredr in which python looks for a method or attribute when you call it on an object. especially when multiple inheritance is innvolved.'''
