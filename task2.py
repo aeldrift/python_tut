473952566e7f974037bd373d46403269a2b26623
Teesha <jindalteesha22@gmail.com>
2025-08-06 10:22:55 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,28 @@
+''' 🧪 Exercise: University Example:
+
+You're designing a simple system for people at a university.
+
+class Person should store name and print a message in its constructor.
+
+class Student should inherit from Person, store student_id, and also print a message in its constructor.
+
+The Student class should:
+
+Have a method introduce() that:
+
+Calls the inherited greet() method.
+
+Then prints the student ID.
+
+❓Your Tasks:
+
+1. Complete the class definitions below.
+2. Make sure you use super() correctly.
+3. Choose whether greet() should take one or two parameters.
+4. Create an object of the Student class and call introduce().
+🧠 Goal Output:
+
+Person constructor called
+Student constructor called
+Hello, I’m Aarav
+My student ID is SRM2025 '''
