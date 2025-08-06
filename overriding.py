798c17431b3d21cc740f6c8ab7d5fca52c298918
iteesha22 <jindalteesha22@gmail.com>
2025-08-06 21:06:31 +0530

    added

--- overriding.py
+++ overriding.py
@@ -1,5 +1,5 @@
 # OVERRIDING: In this, method greet() in the child that has the same name as the parent’s method.
-# Example:
+# Example1:
 
 class Parent:
     def greet(self):
@@ -12,4 +12,23 @@ class Child(Parent):
 c=Child()
 c.greet()  # This will call the Child's greet method
            # expected output: Hello from Child
-           
+
+# example2:
+class Parent:
+    def show(self):
+        print("Parent class method")
+
+class Child1(Parent):
+    def show(self):   # overridden
+        print("Child1 class overrides Parent")
+
+class Child2(Parent):
+    pass  #it tells p that do nothing here, just continue
+
+
+
+c1 = Child1()
+c2 = Child2()
+
+c1.show()   # Uses Child1's override
+c2.show()   # Uses Parent's method (no override is there)
