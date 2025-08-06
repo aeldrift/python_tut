5f21c4c13594bc9db6fb11110f9fd0c187c2a8b0
Teesha <jindalteesha22@gmail.com>
2025-08-06 14:32:43 +0530

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,2 @@
+# 🧪 Practice Question 
+'''Try this challenge:  1.  Add a method in Animal called eat()  2. Add a method in Dog called run()  3. Create a new class BabyPuppy that inherits from Puppy  4. Create an object of BabyPuppy and call all methods.'''
