7c7bbf4fc2c7cf6414e376ad1ff02f49db82d008
Teesha <jindalteesha22@gmail.com>
2025-08-07 10:18:31 +0530

    Update MRO.py

--- MRO.py
+++ MRO.py
@@ -12,3 +12,26 @@ obj = B()
 obj.show()  # expected output: Class A 
 ''' reason: B inherits from A, so it can access A's methods. Python looks first in:
 Class B and then in class A'''
+
+
+# IN CASE IF HYBRID INHERITANCE:
+
+class A:
+    def greet(self):
+        print("Hello from A")
+
+class B(A):
+    def greet(self):
+        print("Hello from B")
+
+class C(A):
+    def greet(self):
+        print("Hello from C")
+
+class D(B, C):
+    pass
+
+obj = D()
+obj.greet()  # expected output: Hello from B
+
+
