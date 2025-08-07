86592185a3a5fe23df921417253e64422a1d8ae0
Teesha <jindalteesha22@gmail.com>
2025-08-07 14:55:47 +0530

    Create modifiers.py

---
+++ modifiers.py
@@ -0,0 +1,21 @@
+# Access Specifers or Access Modifiers
+
+# Public Modifier:
+# Example1:
+
+class MyClass:
+    def __init__(self):
+        self.name = "Eliana"   # public variable
+
+obj = MyClass()
+print(obj.name)  # Eliana
+
+# Example2:
+
+class Student:
+    def __init__(self):
+        self.college="MMDU"
+obj=Student()
+print(obj.college
+
+
