cd3ddb6a7beef1f4c67ab3d1c3f998b406fce811
Teesha <jindalteesha22@gmail.com>
2025-08-08 12:33:24 +0530

    Update game.py

--- game.py
+++ game.py
@@ -9,3 +9,21 @@ print(n)
 
 import random
 print(random.randint(1,5))
+
+# Game Implementation:
+
+import random
+def check(comp,user):
+    if comp == user:
+        return 0 
+    if comp == 0 and user == 1:
+        return -1
+    if comp == 1 and user == 2:
+        return -1
+    if comp == 2 and user == 0:
+        return -1
+        
+    return 1
+            
+comp=random.randint(0,2)
+user= int(input("choose you opt: \n 0 for Snake \n 1 for Water \n 2 for Gun\n Choose your input: "))
