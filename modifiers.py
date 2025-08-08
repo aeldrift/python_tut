b6f3d2c5969fe69323e4bda42dc4b1de865784e5
Teesha <jindalteesha22@gmail.com>
2025-08-08 11:32:26 +0530

    Update modifiers.py

--- modifiers.py
+++ modifiers.py
@@ -18,4 +18,41 @@ class Student:
 obj=Student()
 print(obj.college. # MM(DU)
 
+# EXAMPLE: public, private, protected:
 
+      class Dog:
+    def __init__(self,name):
+        self.name=name
+        self._mood="happy"
+        self.__secret="i love cats"
+    
+    def bark(self):
+        print("Woof!")
+
+    def _sleep(self):
+        print("Sleeping...")
+
+    def __dream(self):
+        print("Dreaming about bones...")
+d = Dog("Bruno")
+
+print(d.name)       #  Public - works fine
+print(d._mood)      #  Protected - works, but be careful
+# print(d.__secret) #  rivate - will give an error
+
+# 🕵️ But you CAN access it like this (not recommended):
+print(d._Dog__secret)  # Works, but looks weird
+
+d.bark()         #  Public method
+d._sleep()       # ️ Protected method
+# d.__dream()    #  Private method (won't work)
+d._Dog__dream()  #  Works by name mangling
+
+# ecpected output:
+
+Bruno
+happy
+i love cats
+Woof!
+Sleeping...
+Dreaming about bones...
