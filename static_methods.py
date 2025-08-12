4cc31bcaeebc233195b1750f01bb2fd422f47502
Teesha <jindalteesha22@gmail.com>
2025-08-12 12:03:19 +0530

    Update static_methods.py

--- static_methods.py
+++ static_methods.py
@@ -26,3 +26,15 @@ print(TemperatureConverter.Temp(celsius))
 
 # Practise Question:
 '''Create a class Calculator that has a static method called multiply which takes two numbers as input and returns their product. '''
+
+class Calculator:
+    @staticmethod 
+    def Multiply(num1,num2):
+        return num1 * num2
+        
+num1=float(input("Enter 1st number:", ))
+num2=float(input("Enter 2nd number:", ))
+
+result=Calculator.Multiply(num1,num2)
+
+print(f"The product of {num1} and {num2} is: {result}")
