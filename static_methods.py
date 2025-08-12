62f1a94b0a5def281dcb7a46c54235198785a21f
Teesha <jindalteesha22@gmail.com>
2025-08-12 11:41:49 +0530

    Update static_methods.py

--- static_methods.py
+++ static_methods.py
@@ -15,3 +15,11 @@ e.static_method()   # Also works
 
 Example.static_method() # Also works directly from class
   
+# Example2:
+class TemperatureConverter:
+    @staticmethod 
+    def Temp(c):
+        return (c*9/5) +32
+        
+celsius=float(input("Enter Temp in celsius:", ))
+print(TemperatureConverter.Temp(celsius))
