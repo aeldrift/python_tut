68ccef9248cfb9d6350cdbf79b54311bd06156ea
Teesha <jindalteesha22@gmail.com>
2025-08-13 11:18:25 +0530

    Create inst_variables.py

---
+++ inst_variables.py
@@ -0,0 +1,12 @@
+# Instance Variabkes And class variables 
+#Example:
+
+class Car:
+    def __init__(self, brand):
+        self.brand = brand  # instance variable
+
+car1 = Car("Toyota")   # Instance 1
+car2 = Car("Tesla")    # Instance 2
+
+print(car1.brand)  # Toyota
+print(car2.brand)  # Tesla
