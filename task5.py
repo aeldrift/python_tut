67381234b36e92e39b25b67b65084e9a0956abf3
Teesha <jindalteesha22@gmail.com>
2025-08-14 10:08:07 +0530

    Update task5.py

--- task5.py
+++ task5.py
@@ -4,3 +4,23 @@
 write a program to create a library from this library class and show you can 
 print all books, add a bookand get a number of books using different methods. 
 Show that your program dosen't presist the books after the program is stopped! '''
+
+class Library:
+    def __init__(self):
+        self.books = []  # List to store book names
+        self.no_of_books = 0  # Count of books
+
+    def add_book(self, book_name):
+        self.books.append(book_name)
+        self.no_of_books += 1
+
+    def print_books(self):
+        if self.books:
+            print("Books in the library:")
+            for book in self.books:
+                print(f"- {book}")
+        else:
+            print("No books in the library.")
+
+    def get_no_of_books(self):
+        return self.no_of_books
