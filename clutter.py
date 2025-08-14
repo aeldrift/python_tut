3a6329ecf6df8107ed0c6b4882c36c505997c237
Teesha <jindalteesha22@gmail.com>
2025-08-14 11:50:31 +0530

    Update clutter.py

--- clutter.py
+++ clutter.py
@@ -11,4 +11,14 @@ for user in users:
 ''' 1. Don't Repeat Yourself (DRY)
 Repetition = future maintenance pain. Use functions.'''
 
+# Instead of doing this:
+print("Bye, Alice!")
+print("Bye, Bob!")
+
+# Do this:
+def say_goodbye(name):
+    print(f"Bye, {name}!")
+
+say_goodbye("Alice")
+say_goodbye("Bob")
 
