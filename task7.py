98f50e72ed362d0b7a418136b11729ee798d01a3
Teesha <jindalteesha22@gmail.com>
2025-08-22 11:49:49 +0530

    Create task7.py

---
+++ task7.py
@@ -0,0 +1,4 @@
+# Practice Question:
+
+''' OOP + Practical
+“Design a Python class to represent a library book system where each book has a title, author, issue date, and due date. The system should also calculate fines for overdue books. '''
