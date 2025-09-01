8b2d5bcae31fcb1775fb090202c0ab3f93cacec3
Teesha <jindalteesha22@gmail.com>
2025-09-01 11:31:46 +0530

    Update obj_introspecrion.py

--- obj_introspecrion.py
+++ obj_introspecrion.py
@@ -16,6 +16,15 @@ print(dir(x))   # shows all methods/attributes of list
 
 # 2. __dict__	Instance variables in dict form
 # example:
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.version = 1
+
+p = Person("John", 30)
+print(p.__dict__)
+# Output: {'name': 'John', 'age': 30, 'version': 1}
 
 #3. help(obj)	Documentation/help info 
 # example:
