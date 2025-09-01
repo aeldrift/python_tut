acdd969f8a79078419f9b96de4a58849470d15f4
Teesha <jindalteesha22@gmail.com>
2025-09-01 11:27:16 +0530

    Create obj_introspecrion.py

---
+++ obj_introspecrion.py
@@ -0,0 +1,10 @@
+# Introspection
+
+'''Ability of a program to examine the type and properties of an object at runtime.
+
+Simply: "Looking inside an object while the program is running."
+
+Useful for debugging, checking attributes/methods, and building flexible programs. '''
+
+
+
