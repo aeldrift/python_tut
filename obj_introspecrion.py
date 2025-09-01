7b423bb87b9b94255d614f61f20c048beeccdc10
Teesha <jindalteesha22@gmail.com>
2025-09-01 11:29:18 +0530

    Update obj_introspecrion.py

--- obj_introspecrion.py
+++ obj_introspecrion.py
@@ -4,7 +4,10 @@
 
 Simply: "Looking inside an object while the program is running."
 
-Useful for debugging, checking attributes/methods, and building flexible programs. '''
+Useful for debugging, checking attributes/methods, and building flexible programs. 
 
+Its Functions are: 
 
-
+1. dir(obj)	List of all attributes & methods
+2. __dict__	Instance variables in dict form
+3. help(obj)	Documentation/help info '''
