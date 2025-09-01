c40107dbe82865b73cfc277c1af153326ac6f629
Teesha <jindalteesha22@gmail.com>
2025-09-01 11:33:59 +0530

    Update obj_introspecrion.py

--- obj_introspecrion.py
+++ obj_introspecrion.py
@@ -30,3 +30,32 @@ print(p.__dict__)
 # example:
 
 print(help(Person))
+
+
+# Output: 
+'''
+['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getstate__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+{'name': 'John', 'age': 30, 'version': 1}
+Help on class Person in module __main__:
+
+class Person(builtins.object)
+ |  Person(name, age)
+ |
+ |  # 2. __dict__        Instance variables in dict form
+ |  # example:
+ |
+ |  Methods defined here:
+ |
+ |  __init__(self, name, age)
+ |      Initialize self.  See help(type(self)) for accurate signature.
+ |
+ |  ----------------------------------------------------------------------
+ |  Data descriptors defined here:
+ |
+ |  __dict__
+ |      dictionary for instance variables
+ |
+ |  __weakref__
+ |      list of weak references to the object
+
+None  '''
