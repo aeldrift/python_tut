69614ffeda249ba75304da8ab9988f6adbba2a03
iteesha22 <jindalteesha22@gmail.com>
2025-09-04 19:14:29 +0530

    commit to python tutorials

--- overriding.py
+++ overriding.py
@@ -1,36 +1,15 @@
-# OVERRIDING: In this, method greet() in the child that has the same name as the parent’s method.
-# Example1:
+# Overriding: 
+''' Method overriding in Python occurs when a child (subclass) defines a method with the same name and 
+parameters as a method in its parent (superclass). When the method is called on a subclass object, the 
+subclass’s version is executed, replacing (overriding) the parent’s implementation.'''
 
-class Parent:
-    def greet(self):
-        print("Hello from Parent")
+# Example:
 
-class Child(Parent):
-    def greet(self):  # same method name as in Parent
-        print("Hello from Child")
-
-c=Child()
-c.greet()  # This will call the Child's greet method
-           # expected output: Hello from Child
-
-# example2:
 class Parent:
     def show(self):
-        print("Parent class method")
-
-class Child1(Parent):
-    def show(self):   # overridden
-        print("Child1 class overrides Parent")
-
-class Child2(Parent):
-    pass  #it tells p that do nothing here, just continue
-
-c1 = Child1()
-c2 = Child2()
-
-c1.show()   # Uses Child1's override
-c2.show()   # Uses Parent's method (no override is there)
-
-# expected output:
-'''Child1 class overrides Parent
-Parent class method '''
+        print("I am a parent class")
+class Child(Parent):
+    def show(self):
+        print("I am a child class")
+obj = Child()
+obj.show()  # Output: I am a child class (method override)
