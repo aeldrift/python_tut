e3e50c614d4e1a347f00a58ed7e459493e7dbdbc
Teesha <jindalteesha22@gmail.com>
2025-09-07 15:07:57 +0530

    Update task7.py

--- task7.py
+++ task7.py
@@ -1,2 +1,1 @@
-
-
+# TASK:  write a program to manipulate PDF files using PYPDF, your program should be able to merge multiple PDF files into a single PDF. You are welcome to add more functionalities.
